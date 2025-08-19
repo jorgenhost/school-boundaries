fab4e8ae01fdd1dd62d4c4cb2c050f34082df56b
Jørgen Høst <jorgenhost96@gmail.com>
2025-08-19 12:01:21 +0200

    complete repo: all steps to reproduce

--- README.md
+++ README.md
@@ -1,17 +1,76 @@
-# School boundaries
+# School boundaries - tessellation of adresses with Voronoi diagrams
+
+This repo shows how to tessellate individual addresses as their own polygon(s). This can be used as neighborhood definition or other geographic delineation/boundary.
+
+Because Voronoi diagrams can extend to infinity, the idea here is to provide administrative boundaries that "clips"/"coerces" Voronoi diagrams within your administrative border of choice. In this project, I use municipalities. Below is an example of Frederiksberg municipality.
+
+![pic](figs/voronoi_tess_fberg.png)
+
+## Step 0
+Install uv:
+
+Linux/Mac:
+
+```bash
+wget -qO- https://astral.sh/uv/install.sh | sh
+```
+
+Windows
+```bash
+powershell -c "irm https://astral.sh/uv/install.ps1 | more"
+```
+
+Clone repo:
+
+```bash
+git clone https://github.com/jorgenhost/school-boundaries.git
+cd school-boundaries
+```
+
+And install dependencies with uv:
+
+```bash
+uv sync
+```
+
 
 ## Step 1
 
+Fetch all adresses:
 ```bash
-wget https://api.dataforsyningen.dk/adgangsadresser?format=csv
+curl -L -o data/dk_adresser.ndjson "https://api.dataforsyningen.dk/adgangsadresser?&format=csv"
 ```
 
+Example with Frederiksberg (kommune=0147 or 147):
+
+```bash
+curl -L -o data/dk_adresser.csv "https://api.dataforsyningen.dk/adgangsadresser?kommunekode=0147&format=csv"
+```
+
+Fetch shapefiles
+
+```bash
+curl -L -o data/admin_boundaries.zip "https://ftp.sdfe.dk/main.html?download&weblink=60a0dc5e27d9561a3d761e876cb2684d&realfilename=DK%5FAdministrativeUnit%2Ezip"
+
+unzip data/admin_boundaries.zip -d data/
+rm data/admin_boundaries.zip
+
+```
+
+Then play with it here:
 ```python
 import polars as pl
 import polars.selectors as cs
 
-df = pl.scan_csv('adgangsadresser?format=csv').collect(engine = 'streaming').with_columns(
+df = pl.scan_csv('data/dk_adresser.csv').collect(engine = 'streaming').with_columns(
     cs.integer().shrink_dtype()
 )
 df.write_parquet('data/adresser.pq')
+```
+
+Or run the two scripts in the `src`-folder:
+
+```bash
+uv run src/01_parse_voronoi.py
+uv run src/02_plot_voronoi.py
 ```
--- src/01_parse_voronoi.py
+++ src/01_parse_voronoi.py
@@ -10,22 +10,23 @@ import tqdm, glob
 import pandas as pd
 from functools import lru_cache
 
-df = pl.scan_csv('adgangsadresser?format=csv').collect(engine = 'streaming').with_columns(
+df = pl.scan_csv('data/dk_adresser.csv').collect(engine = 'streaming').with_columns(
     cs.integer().shrink_dtype()
 )
-df.write_parquet('data/adresser.pq')
+df.write_parquet('data/dk_adresser.pq')
 
 @lru_cache(maxsize=1)
 def load_kommune_data():
-    gdf_kom = gpd.read_parquet('data/kommune.pq').to_crs(25832)
-    gdf_kom['code'] = gdf_kom['code'].astype('int')
+    gdf_kom = gpd.read_file('data/au_inspire.gpkg').to_crs(25832)
+    gdf_kom = gdf_kom[~gdf_kom['nationalcode'].str.contains('DK')].reset_index(drop=True)
+    gdf_kom['nationalcode'] = gdf_kom['nationalcode'].astype('int')
     return gdf_kom
 
 def parse_voronoi(kom: int):
 
     gdf_kom = load_kommune_data()    
 
-    df = (pl.scan_parquet('data/adresser.pq', low_memory = True)
+    df = (pl.scan_parquet('data/dk_adresser.pq', low_memory = True)
           .filter(pl.col("kommunekode")==kom)
           .select(pl.col("vejnavn", "husnr", "postnr", "kommunekode", "landsdelsnuts3"), pl.col("etrs89koordinat_øst").alias("etrs89_east"), pl.col("etrs89koordinat_nord").alias("etrs89_north"))
           .filter(pl.struct(pl.col("etrs89_east", "etrs89_north")).is_first_distinct())
@@ -36,7 +37,7 @@ def parse_voronoi(kom: int):
     gdf_adr = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(x = df.etrs89_east, y=df.etrs89_north), crs=25832)
 
 
-    kom_shape = gdf_kom[gdf_kom['code']==kom].reset_index(drop=True)
+    kom_shape = gdf_kom[gdf_kom['nationalcode']==kom].reset_index(drop=True)
     gdf_adr['voronoi'] = gdf_adr.voronoi_polygons()
 
     gdf_adr['points'] = gdf_adr['geometry']
@@ -49,25 +50,31 @@ def parse_voronoi(kom: int):
 
     print(f'{kom} to voronoi done.')
 
-    gdf_out.to_parquet(f'data/voronoi/adresser_voronoi_{kom}.pq')
+    gdf_out.to_parquet(f'data/voronoi/dk_adresser_voronoi_{kom}.pq')
 
 
-lf = pl.scan_parquet('data/adresser.pq').select(pl.col("kommunekode").unique())
+lf = pl.scan_parquet('data/dk_adresser.pq').select(pl.col("kommunekode").unique())
 kommunerz = lf.collect().to_series().to_list()
 with concurrent.futures.ThreadPoolExecutor(max_workers = 8) as executor:
     res = list(tqdm.tqdm(executor.map(parse_voronoi, kommunerz)))
 
-vo_list = glob.glob('data/voronoi/adresser_v*.pq')
+files = glob.glob('data/voronoi/dk_adresser_v*.pq')
 
-gdf = gpd.read_parquet(vo_list[0])
+def concat_voronoi_data(list: list[str] | str):
+    if len(list)==1:
+        gdf = gpd.read_parquet(list[0])
+        gdf = gdf.reset_index(drop = True)
+        gdf.to_parquet('data/dk_adresser_voronoi.pq')
+    else:
+        gdf = gpd.read_parquet(list[0])
+        for data in list:
+            if data == list[0]:
+                pass
+            gdf2 = gpd.read_parquet(data)
 
-for data in vo_list:
-    if data == vo_list[0]:
-        pass
-    gdf2 = gpd.read_parquet(data)
+            gdf = pd.concat([gdf, gdf2])
 
-    gdf = pd.concat([gdf, gdf2])
+            gdf = gdf.reset_index(drop = True)
+        gdf.to_parquet('data/dk_adresser_voronoi.pq')
 
-gdf = gdf.reset_index(drop = True)
-
-gdf.to_parquet('data/adresser_voronoi.pq')
+concat_voronoi_data(files)
